d0f18e112c37f9eff42982f84edc7f8e234c8457
MASANORI-M <77526005+MASANORI-M@users.noreply.github.com>
2021-02-28 23:20:06 +0900

    up3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColofulMsg from "./components/ColorfulMsg";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShow, setFace] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickShow = () => {
+    setFace(!faceShow);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShow || setFace(true);
+    } else {
+      faceShow && setFace(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんちは!</h1>
       <ColofulMsg color="blue">Youは元気かい！</ColofulMsg>
       <ColofulMsg color="pink">元気だよん♡</ColofulMsg>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickShow}>on/off</button>
       <p>{num}</p>
+      {faceShow && <p>(ﾟДﾟ)ﾉ</p>}
     </>
   );
 };
